d191b26a2f4770ccccdcce81458734f413157f81
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2022-09-16 20:53:30 +0530

    Add files via upload

---
+++ DSA_450_problems/rotate_by_1.java
@@ -0,0 +1,25 @@
+class Compute {
+
+    
+
+    public void rotate(int arr[], int n)
+
+    {
+
+        int temp = arr[n-1];
+
+        
+
+        for(int i=n-1;i>=1;i--){
+
+            arr[i] = arr[i-1];
+
+        }
+
+        arr[0]= temp;
+
+    }
+
+    
+
+}
