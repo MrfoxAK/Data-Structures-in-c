95ffe1951291aca96902254eabb8f17c6b5327bc
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2022-10-15 21:33:10 +0530

    Add files via upload

---
+++ Tree/BST/search_in_BST.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+     int data;
+     struct node* left;
+     struct node* right;
+};
+
+void inOrder_traversal(struct node * root){
+     if (root!=NULL)
+     {
+          inOrder_traversal(root->left);
+          printf("%d ",root->data);
+          inOrder_traversal(root->right);
+     }
+}
+
+struct node* insert(struct node* root,int data){
+     if (root==NULL)
+     {
+          struct node* n;
+          n=(struct node*)malloc(sizeof(struct node));
+          n->data=data;
+          n->left=NULL;
+          n->right=NULL;
+          root = n;
+          // return root;
+     }
+     else if (data<root->data)
+     {
+          root->left = insert(root->left,data);
+     }
+     else
+     {
+          root->right = insert(root->right,data);
+     }
+     return root;
+}
+
+struct node* search(struct node* root,int data){
+     if (root==NULL)
+     {
+          return NULL;
+     }
+     else if (root->data==data)
+     {
+          return root;
+     }
+     else if (data<root->data)
+     {
+         return search(root->left,data);
+     }
+     else
+     {
+          return search(root->right,data);
+     }
+}
+
+int main()
+{
+     struct node* root=NULL;
+     root = insert(root, 15);
+     root = insert(root, 10);
+     root = insert(root, 20);
+     root = insert(root, 25);
+     root = insert(root, 8);
+     root = insert(root, 12);
+     inOrder_traversal(root);
+     if (search(root,12))
+     {
+          printf("YES\n");
+     }
+     else
+     {
+          printf("NO\n");
+     }
+     return 0;
+}
