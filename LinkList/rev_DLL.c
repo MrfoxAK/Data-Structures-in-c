eae42557b1cf7f0b4bee1450919bb252d2cc59fa
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2022-11-30 13:05:01 +0530

    Add files via upload

---
+++ LinkList/rev_DLL.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+     int data;
+     struct node *next;
+     struct node *pre;
+};
+
+void linklisttreversal(struct node *ptr)
+{
+     struct node* last;
+     printf("\nTraversal in forward direction \n");
+     while (ptr != NULL)
+     {
+          printf("%d ", ptr->data);
+          last = ptr;
+          ptr = ptr->next;
+     }
+     printf("\n");
+     printf("\nTraversal in reverse direction \n");
+     while (last!=NULL)
+     {
+          printf("%d ",last->data);
+          last = last->pre;
+     }
+}
+
+struct node *createDoubly_linklist(struct node *head, int n)
+{
+     struct node *temp;
+     struct node *p;
+     for (int i = 0; i < n; i++)
+     {
+          temp = (struct node *)malloc(sizeof(struct node));
+          printf("Enter data: ");
+          scanf("%d", &temp->data);
+          temp->pre = NULL;
+          temp->next = NULL;
+          if (head == NULL)
+          {
+               head = p = temp;
+          }
+          else
+          {
+               p->next = temp;
+               temp->pre = p;
+               p=temp;
+          }
+     }
+     return head;
+}
+
+struct node* reverse_doubly_linklist(struct node* head){
+     struct node* cp=head;
+     struct node* np=head;
+     struct node* temp=NULL;
+     while (cp!=NULL)
+     {
+          np = cp->next;
+          cp->next = temp;
+          cp->pre = np;
+          temp = cp;
+          cp = np;
+     }
+     return temp;
+}
+
+int main()
+{
+     int n;
+     struct node *head = NULL;
+     printf("How many nodes u want: ");
+     scanf("%d", &n);
+     head = createDoubly_linklist(head, n);
+     linklisttreversal(head);
+     head = reverse_doubly_linklist(head);
+     linklisttreversal(head);
+     return 0;
+}
+
+
