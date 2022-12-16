f54dfa579283208e55a83a810b000d251effef99
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2022-12-16 23:53:46 +0530

    Add files via upload

---
+++ Tree/Binary_Tree.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct node
+{
+     int data;
+     node *left;
+     node *right;
+};
+
+node *createNewNode(int data)
+{
+     node *newNode = new node();
+     if (!newNode)
+     {
+          cout << "Memory error\n";
+     }
+     newNode->data = data;
+     newNode->left = newNode->right = NULL;
+     return newNode;
+}
+
+node *insert(node *root, int data)
+{
+     if (root==NULL)
+     {
+          root = createNewNode(data);
+          return root;
+     }
+     queue<node*>q;
+     q.push(root);
+     while (!q.empty())
+     {
+          node* temp = q.front();
+          q.pop();
+          if (temp->left!=NULL)
+          {
+               q.push(temp->left);
+          }
+          else{
+               temp->left = createNewNode(data);
+               return root;
+          }
+          if (temp->right != NULL)
+               q.push(temp->right);
+          else
+          {
+               temp->right = createNewNode(data);
+               return root;
+          }
+     }
+}
+
+void inorder(node *temp)
+{
+     if (temp == NULL)
+          return;
+
+     inorder(temp->left);
+     cout << temp->data << ' ';
+     inorder(temp->right);
+}
+
+int main()
+{
+     node *root = NULL;
+     root = insert(root, 1);
+     root = insert(root, 2);
+     root = insert(root, 3);
+     root = insert(root, 4);
+     root = insert(root, 5);
+     inorder(root);
+     cout<<"\nRoot is ";
+     cout<<root->data;
+     return 0;
+}
