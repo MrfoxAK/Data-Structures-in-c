5c15b8ec97665591d4b4af8f11ef00bebd7b26d0
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2022-12-21 20:34:12 +0530

    Add files via upload

---
+++ LinkList/rev_Link_list.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+     int data;
+     struct node *next;
+} node;
+
+node * rev_linklist(node * head){
+     node* temp = head;
+     node* ptr = head;
+     node* p = NULL;
+     while (ptr!=NULL)
+     {
+          ptr=ptr->next;
+          temp->next = p;
+          p = temp;
+          temp = ptr;
+     }
+     head = p;
+     return head;
+}
+
+int main()
+{
+     node *head, *newNode, *temp;
+     head = NULL;
+     int choice;
+     while (choice)
+     {
+          newNode = (node *)malloc(sizeof(node));
+          printf("Enter data: ");
+          scanf("%d", &newNode->data);
+          newNode->next = NULL;
+          if (head == NULL)
+          {
+               head = temp = newNode;
+          }
+          else
+          {
+               temp->next = newNode;
+               temp = newNode;
+          }
+          printf("Do you want to continue (1/0) : ");
+          scanf("%d", &choice);
+     }
+     temp = head;
+     while (temp != NULL)
+     {
+          printf("%d-> ", temp->data);
+          temp = temp->next;
+     }
+     head = rev_linklist(head);
+     temp = head;
+     printf("\nrev is : \n");
+     while (temp != NULL)
+     {
+          printf("%d-> ", temp->data);
+          temp = temp->next;
+     }
+     return 0;
+}
