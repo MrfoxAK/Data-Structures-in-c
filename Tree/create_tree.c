cc047fc80a9105ed1748664e26d162888019ff3a
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-01-24 23:09:25 +0530

    Add files via upload

---
+++ Tree/create_tree.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+     int data;
+     struct node* left;
+     struct node* right;
+};
+
+struct node *create_node(int data)
+{
+     struct node *new_node = (struct node *)malloc(sizeof(struct node));
+     new_node->left = NULL;
+     new_node->right = NULL;
+     new_node->data = data;
+     return new_node;
+}
+
+struct node* create_tree(struct node* root){
+     int data;
+     printf("Enter data: ");
+     scanf("%d",&data);
+     if (data==-1)
+     {
+          return 0;
+     }
+     else{
+          root = create_node(data);
+          printf("Left node of %d ",data);
+          root->left = create_tree(root->left);
+          printf("right node of %d ",data);
+          root->right = create_tree(root->right);
+     }
+     return root;
+}
+
+void Postorder_traversal(struct node * root){
+     if (root!=NULL)
+     {
+          Postorder_traversal(root->left);
+          Postorder_traversal(root->right);
+          printf("%d ",root->data);
+     }
+}
+
+int main()
+{
+     struct node* root=NULL;
+     root = create_tree(root);
+     Postorder_traversal(root);
+     return 0;
+}
