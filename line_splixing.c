0d29d0858ad72e6178faa62dca8ff9a2438b5747
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-02-01 22:32:04 +0530

    Add files via upload

---
+++ line_splixing.c
@@ -0,0 +1,12 @@
+// C program to illustrate the concept of Line splicing.
+#include <stdio.h>
+int main()
+{
+	// Line Splicing\
+	printf("Hello GFG\n");
+	printf("welcome\n");
+	/* Example 2 - both of the below lines will be printed*/ \
+	printf("Hello\t");
+	printf("World");
+	return (0);
+}
