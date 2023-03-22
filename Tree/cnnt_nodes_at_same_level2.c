714b03019f2f6b572179bff282358aac4ca902e4
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-03-22 09:55:56 +0530

    Add files via upload

---
+++ Tree/cnnt_nodes_at_same_level2.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_Q_SIZE 500
+
+struct node
+{
+     int data;
+     struct node *left;
+     struct node *right;
+     struct node *newRight;
+};
+
+struct node *newNode(int data)
+{
+     struct node *node = (struct node *)malloc(sizeof(struct node));
+     node->data = data;
+     node->left = NULL;
+     node->right = NULL;
+     node->newRight = NULL;
+     return (node);
+}
+
+int f, r;
+
+struct node **createQueue()
+{
+     struct node **queue = (struct node **)malloc(sizeof(struct node *) * MAX_Q_SIZE);
+     f = r = 0;
+     return queue;
+}
+
+int isEmpty(struct node **q)
+{
+     if (f == r)
+          return 1;
+     else
+          return 0;
+}
+
+void enQueue(struct node **q, struct node *n)
+{
+     q[r] = n;
+     r++;
+}
+
+struct node *deQueue(struct node **q)
+{
+     f++;
+     return q[f - 1];
+}
+
+int Qsize(struct node **q)
+{
+     int c = 0;
+     for (int i = f; i < r; i++)
+     {
+          c++;
+     }
+     return c;
+}
+
+void connect(struct node *root)
+{
+     struct node** q = createQueue();
+     struct node* curr = NULL;
+     enQueue(q,root);
+     enQueue(q,NULL);
+     while (!isEmpty(q))
+     {
+          curr = deQueue(q);
+          if (curr!=NULL)
+          {
+               curr->newRight = q[f];
+               if (curr->left != NULL)
+               {
+                    enQueue(q, curr->left);
+               }
+               if (curr->right != NULL)
+               {
+                    enQueue(q, curr->right);
+               }
+          }
+          else{
+               if (!isEmpty(q))
+               {
+                    enQueue(q,NULL);
+               }
+          }
+     }
+}
+
+int main()
+{
+     struct node *root = newNode(1);
+     root->left = newNode(2);
+     root->right = newNode(3);
+     root->left->left = newNode(4);
+     root->right->left = newNode(40);
+     root->left->right = newNode(5);
+     // root->left->right->right = newNode(10);
+     printf("Level Order traversal of binary tree is \n");
+     // level_order(root);
+     connect(root);
+     printf("\n new %d\n",root->left->right->newRight->data);
+     return 0;
+}
