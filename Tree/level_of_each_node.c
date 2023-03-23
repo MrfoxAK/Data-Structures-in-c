a4b5c0711d3a1bb7f1c50444a8f7ddf9aa362ca2
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-03-23 10:39:45 +0530

    Add files via upload

---
+++ Tree/level_of_each_node.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_Q_SIZE 500
+
+struct node {
+	int data;
+	struct node* left;
+	struct node* right;
+     int level;
+};
+
+struct node* newNode(int data)
+{
+	struct node* node = (struct node*)malloc(sizeof(struct node));
+	node->data = data;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
+}
+
+int f,r;
+
+struct node** createQueue(){
+     struct node** queue = (struct node**)malloc(sizeof(struct node*)*MAX_Q_SIZE);
+     f = r = 0;
+     return queue;
+}
+
+void enQueue(struct node** queue,struct node* n){
+     queue[r] = n;
+     r++;
+}
+
+struct node* deQueue(struct node** queue){
+     f++;
+     return queue[f-1];
+}
+
+int isEmpty(struct node** queue){
+     if (f==r)
+     {
+          return 1;
+     }
+     else{
+          return 0;
+     }
+}
+
+void level_order(struct node* root){
+     static int c=0;
+     struct node** q = createQueue();
+     // struct node* temp_root = root;
+     enQueue(q,root);
+     enQueue(q,NULL);
+     while (!isEmpty(q))
+     {
+          struct node* temp_root = deQueue(q);
+          if (temp_root==NULL)
+          {
+               c++;
+               // printf("%d",c);
+               if(isEmpty(q)){
+                    break;
+               }
+               else{
+                    enQueue(q,NULL);
+                    // printf("\n");
+               }
+          }
+          else{
+               temp_root->level = c;
+               // printf("%d ",temp_root->data);
+               if (temp_root->left != NULL)
+               {
+                    enQueue(q,temp_root->left);
+               }
+               if (temp_root->right != NULL)
+               {
+                    enQueue(q,temp_root->right);
+               }
+          // temp_root = deQueue(q);
+          }
+     }
+}
+
+int main()
+{
+	struct node* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+	// root->left->right->right = newNode(10);
+	printf("Level Order traversal of binary tree is \n");
+	level_order(root);
+     printf("%d ",root->right->level);
+	return 0;
+}
