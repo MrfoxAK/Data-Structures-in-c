110d8cc50477a77a334b4a4c2706f8e9ba9a5663
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-03-31 02:45:09 +0530

    Add files via upload

---
+++ Tree/ancestors_of_a_node.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+     int data;
+     struct node* left;
+     struct node* right;
+};
+
+void inOrder_traversal(struct node * root){
+     if (root!=NULL)
+     {
+          inOrder_traversal(root->left);
+          printf("%d ",root->data);
+          inOrder_traversal(root->right);
+     }
+}
+
+struct node* insert(struct node* root,int data){
+     if (root==NULL)
+     {
+          struct node* n;
+          n=(struct node*)malloc(sizeof(struct node));
+          n->data=data;
+          n->left=NULL;
+          n->right=NULL;
+          root = n;
+          // return root;
+     }
+     else if (data<root->data)
+     {
+          root->left = insert(root->left,data);
+     }
+     else
+     {
+          root->right = insert(root->right,data);
+     }
+     return root;
+}
+
+int ancestors(struct node* root, struct node* target){
+     if (root==NULL)
+     {
+          return 0;
+     }
+     else{
+          if(root->data==target->data){
+               return 1;
+          }
+          if(ancestors(root->left,target) || ancestors(root->right,target)){
+               printf("%d ",root->data);
+               return 1;
+          }
+     }
+}
+
+int main()
+{
+     struct node* root=NULL;
+     root = insert(root, 15);
+     root = insert(root, 10);
+     root = insert(root, 20);
+     root = insert(root, 25);
+     root = insert(root, 8);
+     root = insert(root, 12);
+     // inOrder_traversal(root);
+     ancestors(root, root->right->right);
+     return 0;
+}
