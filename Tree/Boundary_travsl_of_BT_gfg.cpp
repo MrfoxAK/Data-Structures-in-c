1e8059fefebaa618631b141e2edbdc067bd43fdc
Akash Das <95500036+MrfoxAK@users.noreply.github.com>
2023-04-28 20:29:21 +0530

    Add files via upload

---
+++ Tree/Boundary_travsl_of_BT_gfg.cpp
@@ -0,0 +1,73 @@
+/* A binary tree Node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+class Solution
+{
+public:
+     void traverseLeft(Node *root, vector<int> &ans)
+     {
+          if ((root == NULL) || (root->left == NULL && root->right == NULL))
+               return;
+
+          ans.push_back(root->data);
+
+          if (root->left)
+               traverseLeft(root->left, ans);
+          else
+               traverseLeft(root->right, ans);
+     }
+
+     void traverseLeaf(Node *root, vector<int> &ans)
+     {
+          if (root == NULL)
+               return;
+
+          if (root->left == NULL && root->right == NULL)
+          {
+               ans.push_back(root->data);
+               return;
+          }
+
+          traverseLeaf(root->left, ans);
+          traverseLeaf(root->right, ans);
+     }
+
+     void traverseRight(Node *root, vector<int> &ans)
+     {
+          if ((root == NULL) || (root->left == NULL && root->right == NULL))
+               return;
+
+          if (root->right)
+               traverseRight(root->right, ans);
+          else
+               traverseRight(root->left, ans);
+
+          ans.push_back(root->data);
+     }
+
+     vector<int> boundary(Node *root)
+     {
+          vector<int> ans;
+
+          if (root->left == NULL && root->right == NULL)
+          {
+               ans.push_back(root->data);
+               return ans;
+          }
+
+          ans.push_back(root->data);
+
+          traverseLeft(root->left, ans);
+
+          traverseLeaf(root->left, ans);
+          traverseLeaf(root->right, ans);
+
+          traverseRight(root->right, ans);
+
+          return ans;
+     }
+};
